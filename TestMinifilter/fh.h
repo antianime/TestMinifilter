0ece65d7ed175069d36825081ebad5e4fd16318e
antianime_BENREN <1305293412@qq.com>
2025-11-04 21:02:39 +0800

    添加项目文件。

---
+++ TestMinifilter/fh.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <fltKernel.h>
+#include <ntddk.h>
+#include<ntstrsafe.h>
+
+#define MAX_PATH 260
+
+extern PFLT_FILTER FilterHandle;
+
+extern UNICODE_STRING PortName;
+extern PFLT_PORT ServerPort;
+
+extern BOOLEAN FilterSwitch;
+
+NTSTATUS AddPath(PUNICODE_STRING FilePath);
+
+NTSTATUS CommunicationSetup();
+
+struct CommandStruct
+{
+	int ZeroSignal;// = 0;
+	enum CommandEnum
+	{
+		Start = 1,
+		Stop = 2,
+		ClearAll = 3
+	};
+	WCHAR buffer[MAX_PATH];
+};
+
+struct MessageStruct
+{
+	int length;
+	WCHAR buffer[MAX_PATH];
+};
+
+typedef struct _DosNode {	//从C:开始
+	//UCHAR tag;//卷标
+	UNICODE_STRING ParentDir;
+	UNICODE_STRING FileName;
+	struct _DosNode* Next;
+}DosNode, * PDosNode;
+
+typedef struct _DosList {	//从C:开始
+	DosNode* Head;
+	DosNode* Tail;
+	int num;
+}DosList, * PDosList;
+
+
+typedef struct _PathStruct {
+	
+	struct _DosList DosList[26];
+	USHORT SubToDos[64];	//From NTSub to DosSub
+}PathStruct, * PPathStruct;
+
+PathStruct ps;
